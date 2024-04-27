f5e2f39c0b096545f63d1b2a054d2efd9d420cdf
Leiner Barrios <lj.barrios@uniandes.edu.co>
2024-04-27 14:37:07 -0500

    feat: 🎸 Se implementa servicio de vehiculos en componente lista

--- src/app/vehiculo/components/lista-vehiculos/lista-vehiculos.component.ts
+++ src/app/vehiculo/components/lista-vehiculos/lista-vehiculos.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VehiculoService } from '../../vehiculo.service';
+import { Vehiculo } from '../../vehiculo';
 
 @Component({
   selector: 'app-lista-vehiculos',
   standalone: true,
   imports: [],
   templateUrl: './lista-vehiculos.component.html',
-  styles: ``
+  styles: ``,
 })
-export class ListaVehiculosComponent {
+export class ListaVehiculosComponent implements OnInit {
+  public vehiculos: Vehiculo[] = [];
 
+  constructor(private vehiculoService: VehiculoService) {}
+
+  public ngOnInit(): void {
+    this.vehiculoService
+      .obtenerVehiculos()
+      .subscribe((vehiculos) => (this.vehiculos = vehiculos));
+  }
 }
