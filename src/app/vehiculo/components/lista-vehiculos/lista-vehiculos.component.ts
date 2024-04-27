1792f644228589c5183b67e0fbed66a4a8d7b78d
Leiner Barrios <lj.barrios@uniandes.edu.co>
2024-04-27 14:54:48 -0500

    feat: 🎸 Se crea sección para mostrar cantidad de vehículos

--- src/app/vehiculo/components/lista-vehiculos/lista-vehiculos.component.ts
+++ src/app/vehiculo/components/lista-vehiculos/lista-vehiculos.component.ts
@@ -7,16 +7,31 @@ import { Vehiculo } from '../../vehiculo';
   standalone: true,
   imports: [],
   templateUrl: './lista-vehiculos.component.html',
-  styles: ``,
+  styles: `
+    #contenedor-cantidad-vehiculos {
+      width: max-content;
+    }
+  `
 })
 export class ListaVehiculosComponent implements OnInit {
   public vehiculos: Vehiculo[] = [];
+  public listadoDeVehiculos: { marca: string; cantidad: number }[] = [];
 
   constructor(private vehiculoService: VehiculoService) {}
 
   public ngOnInit(): void {
-    this.vehiculoService
-      .obtenerVehiculos()
-      .subscribe((vehiculos) => (this.vehiculos = vehiculos));
+    this.vehiculoService.obtenerVehiculos().subscribe((vehiculos) => {
+      this.vehiculos = vehiculos;
+      this.calcularListadoDeVehiculos();
+    });
+  }
+
+  private calcularListadoDeVehiculos(): void {
+    const cuentas: { [key: string]: number } = this.vehiculos.reduce((acc: { [key: string]: number }, vehiculo) => {
+      acc[vehiculo.marca] = (acc[vehiculo.marca] || 0) + 1;
+      return acc;
+    }, {});
+
+    this.listadoDeVehiculos = Object.entries(cuentas).map(([marca, cantidad]) => ({ marca, cantidad: Number(cantidad) }));
   }
 }
